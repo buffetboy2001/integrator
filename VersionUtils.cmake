9844cad3103db6402d767180edad417d02e74e21
Stuart Bowman <bowman.d.stuart@gmail.com>
2016-03-08 22:10:44 -0500

    updated versioning utils to be fully integrated

--- VersionUtils.cmake
+++ VersionUtils.cmake
@@ -6,11 +6,24 @@
 
 # Get Integrator version macro
 # first param - path to include folder, we will rip version from version.h
-macro(integrator_get_version _include_PATH vmajor vminor vpatch)
+macro(integrator_get_version _include_PATH vmajor vminor vpatch vsuffix)
   file(STRINGS "${_include_PATH}/version.h" _integrator_VER_STRING_AUX REGEX ".*#define[ ]+INTEGRATOR_VERSION_STR[ ]+")
-  string(REGEX MATCHALL "[0-9]+" _integrator_VER_LIST "${_integrator_VER_STRING_AUX}")
+  # message(${_integrator_VER_STRING_AUX}) debugging message
+  string(REGEX MATCHALL "[0-9]+" _integrator_VER_LIST "${_integrator_VER_STRING_AUX}") # version numbers
   list(LENGTH _integrator_VER_LIST _integrator_VER_LIST_LEN)
-# get each version from list
+
+  # suffix processing
+  string(REGEX MATCH "[\"A-Z,a-z\")]*$" _integrator_VER_SUFFIX "${_integrator_VER_STRING_AUX}") # version suffix (string)
+  string(LENGTH ${_integrator_VER_SUFFIX} _integrator_SUFFIX_LENGTH)
+  if(_integrator_SUFFIX_LENGTH EQUAL 1)
+    set(_integrator_VER_SUFFIX " ")
+  else()
+      string(REPLACE ")" "" _integrator_VER_SUFFIX ${_integrator_VER_SUFFIX}) # trim the suffix
+  endif()
+  # message(${_integrator_VER_SUFFIX}) # debugging message
+
+# get each version value from the list and return
+  set(${vsuffix} ${_integrator_VER_SUFFIX})
   if(_integrator_VER_LIST_LEN EQUAL 3)
     list(GET _integrator_VER_LIST 0 ${vmajor})
     list(GET _integrator_VER_LIST 1 ${vminor})
